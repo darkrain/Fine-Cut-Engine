bf2af4b30959bce8b34b86094ca8114c4d08154a
went.out <went.out@gmail.com>
2012-05-03 14:16:42 +0400

    UI

--- _js/markitup/latest/markitup/sets/html/set.js
+++ _js/markitup/latest/markitup/sets/html/set.js
@@ -13,7 +13,6 @@ mySettings = {
 	onShiftEnter:	{keepDefault:false, replaceWith:'<br />\n'},
 	onCtrlEnter:	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>\n'},
 	onTab:			{keepDefault:false, openWith:'\t'},
-	resizeHandle: true,
 	markupSet: [
 		{name:'Heading 1', key:'1', openWith:'<h1(!( class="[![Class]!]")!)>', closeWith:'</h1>', placeHolder:'Your title here...' },
 		{name:'Heading 2', key:'2', openWith:'<h2(!( class="[![Class]!]")!)>', closeWith:'</h2>', placeHolder:'Your title here...' },
