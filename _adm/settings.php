549fb74e6040a9279b40919fd0b82317a685d232
went.out <went.out@gmail.com>
2012-07-25 19:00:26 +0400

    commits from master

--- _adm/settings.php
+++ _adm/settings.php
@@ -4,23 +4,26 @@
 	$perm_folder = 0755;
 	$perm_file = 0644;
 	$pages = 'pages';
-    $static = 'static';
+	$static = 'static';
 	
 	
-    $prepath = '';
-    // this is MAIN SETTINGS for service url discovery!
+	$prepath = '';
+	// this is MAIN SETTINGS for service url discovery!
 	// $deep = '/\/finecut/'; // REG EXP !
-    $deep = '/'.preg_replace( '/\//' , '\\/' , $prepath ).'/';
+	
+	$deep = '/'.preg_replace( '/\//' , '\\/' , $prepath ).'/';
+	
 	/*
 	Examples:
-	$deep = '//';  							// this is default empty URI like http://example.com/ ( or http://example.com )
+	$deep = '//';  					// this is default empty URI like http://example.com/ ( or http://example.com )
 	$deep = '/\/afa\/zxc\/someelse/';		// for http://example.com/afa/zxc/someelse or with /
-	$deep = '/\/zxc/';						// for http://example.com/zxc or with /
+	$deep = '/\/zxc/';				// for http://example.com/zxc or with /
 	*/
 	
 	
 	// this is only for saving in _adm because non Latin files are not supported here!
 	// $fileNameEncoding = 'windows-1251';
+	
 	$fileNameEncoding = "UTF-8";
 	
 	$microtimeEcho = true;
