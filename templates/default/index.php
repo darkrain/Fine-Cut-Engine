3dfbcddf0d65dd09611d2333d90657c6bf0ed05a
went.out <went.out@gmail.com>
2012-08-13 15:40:17 +0400

    fix

--- templates/default/index.php
+++ templates/default/index.php
@@ -105,9 +105,12 @@
 						<br>
 
 						<hr>
+ <?php if( $success['blocks'][0]->value !== '' ){ ?>
 						<pre>
 So, this is all about <?php echo  $success['blocks'][0]->value; ?>
+
 						</pre>
+ <?php } ?>
 					</div>
 				</div>
 				<div class = "span2">
