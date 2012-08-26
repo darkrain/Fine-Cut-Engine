85561109b7d5f82bc63ef765f9b91774ef5efeec
went <went.out>
2012-08-26 18:57:19 +0400

    texts

--- templates/anotherLeftMenu/index.php
+++ templates/anotherLeftMenu/index.php
@@ -30,7 +30,7 @@
 				<table border = "0">
 				<tr>
 				<td>
-					<?php if( !$success['isMain'] ) { echo '<a href = "http://'.$_SERVER['HTTP_HOST'].$success['deep'].'">'; } ?>
+					<?php if( !$success['isMain'] ) { echo '<a href = "/">'; } ?>
 					<image border = "0" src = "<?php echo $success['deep']; ?>/stylesheets/img/logo.png" width = "100" height = "100"
 						alt = "FineCut Logo" class = "logo">
 					<?php if( !$success['isMain'] ) { echo '</a>'; } ?>
@@ -64,7 +64,7 @@
 									echo '<li class="divider-vertical"></li>';
 								 } ?>
 								<li>
-									<form class = "navbar-search" action = "<?php echo $success['deep']; ?>/search/"  method = "POST">
+									<form class = "navbar-search" action = "<?php echo $success['deep']; ?>/search/"  method = "GET">
 										<input type="text" class="search-query span2" placeholder="Search" name = "q">
 										<span class = "icon-search" id = "searchButton"></span>
 									</form>
@@ -127,4 +127,4 @@ So, this is all about <?php echo  $success['blocks'][0]->value; ?>
 		<br>
 
 	</body>
-</html><!-- -->
+</html>
