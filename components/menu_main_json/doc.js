6d612c77bd2bf026cb029f55502a139bf063f341
went <went.out>
2012-09-01 03:04:27 +0400

    support link

---
+++ components/menu_main_json/doc.js
@@ -0,0 +1,23 @@
+[
+	{
+		"title": "About",
+		"type": "link",
+		"link": "/about/"
+	}, {
+		"title": "How to Install",
+		"type": "link",
+		"link": "/install/"
+	}, {
+		"title": "Documentation",
+		"type": "link",
+		"link": "/documentation/"
+	}, {
+		"title": "Download",
+		"type": "link",
+		"link": "/download/"
+	}, {
+		"title": "Help",
+		"type": "link",
+		"link": "/documentation/resources/"
+	}
+]
